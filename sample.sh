cf0f68caae62ca3a106e32e51dbb06c90959acae
k-onishi <princeontrojanhorse@gmail.com>
2018-12-07 14:15:50 +0900

    made a script to set up root file system

--- sample.sh
+++ sample.sh
@@ -2,8 +2,4 @@
 
 set -eu
 
-if [ $# -ge 1 ]; then
-    echo "number of arguments >= 1"
-else
-    echo "number of arguments < 1"
-fi
+echo $EUID
--- setup_fs.sh
+++ setup_fs.sh
@@ -6,22 +6,39 @@ set -eu
 # return: 
 # arg 1: message
 perror_exit() {
-    echo "$1" >&2
+    echo -e "\n$1" >&2
     exit 1
 }
 
+echo -n ">> checking argments..."
 if [ $# -lt 1 ]; then
     perror_exit "you need to designate container name"
 fi
+echo "done."
+
+echo -n ">> checking if this is running on the root permission..."
+if ! [ $EUID -eq 0 ]; then
+    perror_exit "you need run this script as a root"
+fi
+echo "done."
 
 CONTAINER_DIR="$(pwd)/$1"
 ROOT_FS="$CONTAINER_DIR/rootfs"
 
+echo -n ">> creating a directory for..."
 mkdir -p $ROOT_FS
+echo "done."
 
+echo -n ">> cheking if docker is installed..."
 IS_DOCKER_INSTALLED=$(command -v docker)
-if [ ! -z $IS_DOCKER_INSTALLED ]; then
+if [ -z $IS_DOCKER_INSTALLED ]; then
+    rm -rf $CONTAINER_DIR
     perror_exit "you need to install docker."
 fi
+echo "done."
 
-echo "success"
+echo -n ">> creating container an image..."
+docker export $(docker create busybox) | tar -C $ROOT_FS -xvf - > /dev/null
+echo "done."
+
+echo ">> file system is successfully created."
